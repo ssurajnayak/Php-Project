fdcff22298f47bd9ee60189312b1a57fb30e47c2
ssurajnayak <s.surajnayak@gmail.com>
2019-10-01 21:06:46 +0530

    10/1/2019 9.06 pm

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+<div class="row">
+<div class="col-2">
+<img src="images/logo.png">
+</div>
+<div class="col">
+<div class="row" style="padding:10px 0px 0px 0px" >
+<div class="col">
+<h1>IMP.INFO</h1>
+</div>
+<div class="col  " >
+<div class="float-right row" style="padding:10px 15px 0px 0px;">
+
+<div style="height:27px; width:90px; background-color:#00b0f0; border:1px solid #bfb198; border-radius:6px; text-align:center;">
+<a href="#" style=" color:#ffffff; font-weight:bold;font-size:18px;" >EDIT</a>
+</div>
+
+<div style="height:27px; width:90px; background-color:#00b0f0; border:1px solid #bfb198; border-radius:6px; text-align:center; margin-left:10px;">
+<a href="#" style=" color:#ffffff; font-weight:bold;font-size:18px;" >SAVE</a>
+</div>
+</div>
+</div>
+</div>
+<div class="row">
+<div class="col">
+<div class="form-group">
+<input class="form-control form-control-lg"  style=" border-radius:0px; border:2px solid #9b320e;" type="text" >
+</div>
+</div>
+</div>
+
+</div>
+</div>
+<div align="center" class="border-bottom-0" style="border:2px solid #855d5d; text-align:center; font-size:20px; font-weight:800;">
+WT.CAT.GROUP -PARTICIPANT-BRONZE- SILVER -GOLD
+</div>
+<div align="center" style=" border:2px solid #855d5d; text-align:center; font-size:20px; font-weight:800;">
+<div class="row">
+<div class="col">
+<div class="row">
+<div class="col">
+<div class="form-group" style="margin-bottom:0px;">
+<input  style=" border-radius:0px; border:2px solid #d34817; width:90px; height:50px; margin:15px 0px 10px 0px;" type="text" >
+</div>
+</div>
+<div class="col">
+<div class="form-group" style="margin-bottom:0px;">
+<input  style=" border-radius:0px; border:2px solid #9b320e; width:90px; height:50px; margin:15px 0px 10px 0px;" type="text" >
+</div>
+</div>
+<div class="col">
+<div class="form-group" style="margin-bottom:0px;">
+<input  style=" border-radius:0px; border:2px solid #9b320e; width:180px; height:130px; margin:15px 0px 10px 0px;" type="text" >
+</div>
+</div>
+</div>
+</div>
+<div class="col">
+<div class="row">
+<div class="col">
+<div class="form-group" style="margin-bottom:0px;">
+<input  style=" border-radius:0px; border:2px solid #9b320e; width:100px; background-color: #d34817; height:35px;margin:15px 0px 10px 0px;" type="text" >
+</div>
+</div>
+<div class="col">
+<div class="form-group" style="margin-bottom:0px;">
+<input  style=" border-radius:0px; border:2px solid #9b320e; width:100px; background-color: #e9e6e7;  height:35px;margin:15px 0px 10px 0px;" type="text" >
+</div>
+</div>
+<div class="col">
+<div class="form-group" style="margin-bottom:0px;">
+<input  style=" border-radius:0px; border:2px solid #9b320e; width:100px; background-color: #ffff00; height:35px;margin:15px 0px 10px 0px;" type="text" >
+</div>
+</div>
+</div>
+<div class="row">
+<div class="col " >
+<div class="float-right row" style="padding:10px 15px 0px 0px;">
+<div style="height:40px; width:90px; background-color:#9f9789; border:1px solid #ffffff; border-radius:6px; text-align:center; margin:10px;">
+<a href="#" style=" color:#000000; font-weight:bold;font-size:18px; line-height:40px; " >EDIT</a>
+</div>
+<div style="height:40px; width:90px; background-color:#9f9789; border:1px solid #ffffff; border-radius:6px; text-align:center; margin:10px;">
+<a href="#" style=" color:#000000; font-weight:bold;font-size:18px; line-height:40px; " >SAVE</a>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
